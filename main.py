7f3c67af07b928d0d39844d2f8e1b4069cb8a482
Ubuntu <ubuntu@ip-172-31-41-161.eu-west-1.compute.internal>
2018-05-07 14:11:39 +0000

    server update

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ logger = utils.get_logger(__name__)
 
 env = env_creator.create_environment(game='SonicTheHedgehog-Genesis', state='GreenHillZone.Act1')
 actions = actions_builder.build(env.action_space)
-dqn = DQN(env.observation_space, actions, epsilon=0.15, lr=0.001)
+dqn = DQN(env.observation_space, actions, epsilon=0.2, gamma=0.995, lr=0.00001, keras_verbose=0)
 env.close()
 
 #trainer.train_on_random_movie(dqn)
@@ -21,11 +21,12 @@ def create_random_env():
     return env_creator.create_environment(game, state)
 
 for i in range(100):
-    for i in range(10):
+    for i in range(0):
         trainer.train_on_random_movie(dqn)
     
     env = create_random_env()
     trainer.train_on_env(dqn, env, epochs=50, render=False,
+                         manual_interventions_enabled=False,
                          manual_intervention_epsilon=0.8,
                          manual_intervention_duration=200)
     env.close()
